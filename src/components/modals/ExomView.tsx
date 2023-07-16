7eb6c794180d8f6faaea3924aec95ea656fc6266
LiwanagJustine <justine.t.liwanag@gmail.com>
2023-07-16 16:53:45 +0800

    update-exom-modal

--- src/components/modals/ExomView.tsx
+++ src/components/modals/ExomView.tsx
@@ -57,7 +57,7 @@ const ExomView = ({onClose}:Props) => {
                 <div className='max-w-[1100px] h-[650px] w-full m-auto py-16 px-4 relative group'>
                     <div
                         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-                        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+                        className='w-full h-full rounded-2xl bg-center md:bg-cover bg-contain bg-no-repeat duration-500'
                     ></div>
                     {/* Left Arrow */}
                     <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>

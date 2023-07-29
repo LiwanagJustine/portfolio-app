4f18b53b11f59547c2013a056e6b909e1b2b6074
liwanag justine <justine.t.liwanag@gmail.com>
2023-07-29 13:15:32 +0800

    update-contact

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,37 +1,53 @@
 // import { useState } from 'react';
 import './Contact.css'  
-import EmailSender from '../../utils/emailSender/EmailSender'; // Adjust the path as per your file structure
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const sendEmail = (e: any) => {
+        e.preventDefault();
     
-        const formData = new FormData(event.currentTarget);
-        const emailData = {
-          SecureToken: '9fc0adc5-d887-4005-8e73-8855a9e56471',
-          To: 'justine.t.liwanag@gmail.com',
-          From: formData.get("email") as string,
-          Subject: "This is the subject",
-          Body: formData.get("message") as string,
-        };
+        const errorDiv = document.querySelector('.error');
+        const successDiv = document.querySelector('.success');
     
-        EmailSender.send(emailData)
-          .then((message) => alert(message))
-          .catch((error) => alert(error));
-      };
+        emailjs
+          .sendForm('service_dd1fg1s', 'template_be9rsov', e.target, 'wq1jftqFOPkZbhtXr')
+          .then((response) => {
+            successDiv?.classList.remove('hidden');
+    
+            setTimeout(() => {
+              successDiv?.classList.add('hidden');
+            }, 3000);
 
+            e.target.reset();
+          })
+          .catch((error) => {
+
+            errorDiv?.classList.remove('hidden');
+    
+            setTimeout(() => {
+              errorDiv?.classList.add('hidden');
+            }, 3000);
+            e.target.reset();
+          });
+      };
     return (
         <div>
             <div className="contact-container h-fit pb-10">
                 <h3 className='mx-auto md:text-5xl font-semibold text-blue-600 w-fit h-fit py-3 px-20 text-4xl rounded-full flex items-center mb-5'> <span>{"<"}</span> Contact <span>{"/>"}</span></h3>
-                <div className="contact-content w-11/12 md:flex md:flex-row flex flex-col h-fit shadow-xl gap-5 bg-slate-50 dark:shadow-slate-600 dark:shadow-lg mx-auto md:rounded-xl rounded-md p-3 md:p-0 ">
+                <div className="relative contact-content w-11/12 md:flex md:flex-row flex flex-col h-fit shadow-xl gap-5 bg-slate-50 dark:shadow-slate-600 dark:shadow-lg mx-auto md:rounded-xl rounded-md p-3 md:p-0 ">
                     <div className="contact-cont md:w-1/2 md:p-12 p-5">
+                        <div className="error w-full p-3 shadow-md rounded bg-red-500 mb-2 hidden">
+                            <p className='text-xl font-semibold text-white'>Send Failed</p>
+                        </div>
+                        <div className="success w-full mb-2 rounded bg-green-500 shadow-md p-3 hidden">
+                            <p className='text-xl font-semibold text-white'>Send Successful</p>
+                        </div>
                         <h4 className='text-center md:text-left text-slate-700 font-bold text-4xl mb-4'>Message Me!</h4>
-                        <form action="" className="form flex flex-col gap-5" method='post' onSubmit={handleSubmit}>
-                            <input className='rounded border-none shadow-lg p-3 pl-5 ' type="text" id='name' name='name' placeholder='Name' required/>
-                            <input className='rounded border-none shadow-lg p-3 pl-5' type="email" id='email' name='email' placeholder='Email' required/>
-                            <textarea className='rounded border-none shadow-lg p-3 pl-5' name="message" id="message" placeholder='Message' cols={50} rows={4}></textarea>  
+                        <form action="" className="form flex flex-col gap-5" method='post' onSubmit={sendEmail}>
+                            <input className='rounded border-none shadow-lg p-3 pl-5 ' type="text" id='name' name='name' placeholder='Name' required />
+                            <input className='rounded border-none shadow-lg p-3 pl-5' type="email" id='email' name='email' placeholder='Email' required />
+                            <textarea className='rounded border-none shadow-lg p-3 pl-5' name="message" id="message" placeholder='Message' cols={50} rows={4} ></textarea>  
                             <button type='submit' id='submit' className='bg-blue-600 text-white font-semibold text-lg rounded-full mx-auto w-1/2 p-3 hover:bg-blue-700 hover:shadow-lg'>Send</button>             
                         </form>
                     </div>

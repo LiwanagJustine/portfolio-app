4464942331cda933bf2fde07063dbcdb435bc28f
LiwanagJustine <134621763+LiwanagJustine@users.noreply.github.com>
2023-08-16 13:04:06 +0800

    Update About.tsx

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -16,7 +16,7 @@ const About = () => {
                         <img className="w-4/6 rounded-full" src={imageUrl} alt="" />
                     </div>
                     <div className="right md:w-1/2 md:p-14  my-auto">
-                        <h3 className="md:text-6xl font-bold text-slate-700 text-5xl">About me</h3>
+                        <h3 className="md:text-6xl font-bold text-slate-700 text-5xl dark:text-white">About me</h3>
                         <h4 className="md:text-4xl font-bold text-blue-600 md:mb-3 text-3xl mb-2">Frontend Developer</h4>
                         <hr />
                         <p className={btnClick === true ? "md:text-md text-slate-600 md:mt-6 h-fit mt-3 text-md" : "md:text-md text-slate-600 md:mt-6 h-48 overflow-hidden mt-3 md:h-24 md:overflow-hidden"}>Hello and welcome to my web portfolio! I'm a passionate frontend developer with a strong love for creating visually appealing and user-friendly websites. I strive to bring ideas to life through elegant and intuitive user interfaces.My journey as a frontend developer began when I discovered my passion for coding. Since then, I have honed my skills in HTML, CSS, and JavaScript, Bootstrap staying up-to-date with the latest web technologies and best practices. I enjoy the process of turning static designs into dynamic and interactive web experiences.</p>
